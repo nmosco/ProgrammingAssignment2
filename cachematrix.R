8e86a2942ba176576db1c7d05c338b7d37394b2f
Your Name <you@example.com>
2018-03-12 10:24:05 -0300

    - Renaming variables to make the code more readeable. - Adding comments to describe the functionality.

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,35 @@
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(the_inverse) m <<- the_inverse
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+# Creates a "cacheble matrix" object that's capable of caching it's own inverse
+# Provides conveniece functions to get and set values
+
+makeCacheMatrix <- function(originalMatrix = matrix()) {
+    inverseMatrix <- NULL
+    set <- function(newMatrix) {
+        originalMatrix <<- newMatrix
+        inverseMatrix <<- NULL
+    }
+    get <- function() originalMatrix
+    setinverse <- function(the_inverse) inverseMatrix <<- the_inverse
+    getinverse <- function() inverseMatrix
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-cacheSolve <- function(x) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+# Returns the cached value for the inverted matrix (if exists).
+# Otherwise It Calculates the inverse of the matrix and saves it to the cache.
+
+cacheSolve <- function(originalCacheMatrix) {
+    # Gets the cached value for the inverse of the matrix from the cached matrix object.
+    inverseMatrix <- originalCacheMatrix$getinverse()
+
+    # If it exists, return the cached value.
+    if(!is.null(inverseMatrix)) {
+        message("getting cached data")
+        return(inverseMatrix)
+    }
+    # If it doesn't exist, calculate the value, save it to the cache and return the value.
+    originalMatrix <- originalCacheMatrix$get()
+    inverse <- solve(originalMatrix)
+    originalCacheMatrix$setinverse(inverse)
+    inverse
 }
